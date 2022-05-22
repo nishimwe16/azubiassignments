8236e5ee7c95f6622ec375d89e8e77742c2d774d
nishimwe16 <princeelysee@gmail.com>
2022-05-22 13:27:58 +0200

    Final

---
+++ workingwithnumbers.sh
@@ -0,0 +1,12 @@
+//HackerRank link
+//https://www.hackerrank.com/challenges/bash-tutorials---the-world-of-numbers/submissions/code/271189152
+
+//Reading number inputs
+read x
+read y
+
+//Applying arithmetic operations on read numbers
+echo $(expr $x + $y)
+echo $(expr $x - $y)
+echo $(expr $x \* $y)
+echo $(expr $x / $y)
