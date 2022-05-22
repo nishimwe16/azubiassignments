2634c303b076f2b0d028fd37b6bf9351d39d4bb8
nishimwe16 <princeelysee@gmail.com>
2022-05-22 16:35:59 +0200

    Final

---
+++ looping.sh
@@ -0,0 +1,15 @@
+#Hackerrank Link
+#https://www.hackerrank.com/challenges/bash-tutorials---looping-and-skipping/submissions/code/271199536
+
+#Github Repo
+#
+
+#This condition iterates the number parameters in the following way:
+#First parameter is the starting number
+#Second parameter is the ending number
+#Third parameter is the step - interval which the loop will be skipping: In this case, startig from 1 (being an odd number) and a step of 2 ensures we will only be selecting numbers that are not divisible by two, hence being odd numbers
+
+for number in {1..99..2}
+do
+   echo $number
+done
